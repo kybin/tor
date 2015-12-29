3f6fbe0c2d1461daf4cb709fd9d9b09970327188
yongbin kim <kybinz@gmail.com>
2015-12-30 07:52:25 +0900

    prepare ModeType

--- mode.go
+++ mode.go
@@ -4,9 +4,19 @@ import (
 	term "github.com/nsf/termbox-go"
 )
 
+type ModeType int
+
+const (
+	ModeUnknown ModeType = iota
+	ModeNormal
+	ModeFind
+	ModeGotoline
+)
+
 // Mode interface takes an event from terminal,
 // then creates actions, let main loop to do something.
 // TODO: also take tor information.
 type Mode interface {
 	Handle(term.Event) ([]*Action, error)
 }
+
