5d8bfb73d3b92d47b075b29890453029f703d03f
kim yongbin <kybinz@gmail.com>
2017-11-23 22:26:56 +0900

    add Range and use it for Selection

--- selection.go
+++ selection.go
@@ -1,19 +1,18 @@
 package main
 
 type Selection struct {
-	on    bool
-	start Point
-	end   Point
+	on  bool
+	rng Range
 
 	text *Text
 }
 
 func (s *Selection) SetStart(p Point) {
-	s.start = p
+	s.rng.SetStart(p)
 }
 
 func (s *Selection) SetEnd(p Point) {
-	s.end = p
+	s.rng.SetEnd(p)
 }
 
 // Lines return selected line numbers as int slice.
@@ -22,39 +21,23 @@ func (s *Selection) Lines() []int {
 	if !s.on {
 		return nil
 	}
-	start, end := s.MinMax()
-
-	endL := end.l
-	if s.end.o == 0 {
-		endL--
-	}
-
-	lns := make([]int, 0)
-	for l := start.l; l <= endL; l++ {
-		lns = append(lns, l)
-	}
-	return lns
+	return s.rng.Lines()
 }
 
 func (s *Selection) Min() Point {
-	if (s.start.l > s.end.l) || (s.start.l == s.end.l && s.start.o > s.end.o) {
-		return s.end
-	}
-	return s.start
+	return s.rng.Min()
 }
 
 func (s *Selection) Max() Point {
-	if (s.start.l > s.end.l) || (s.start.l == s.end.l && s.start.o > s.end.o) {
-		return s.start
-	}
-	return s.end
+	return s.rng.Max()
 }
 
 func (s *Selection) MinMax() (Point, Point) {
-	if (s.start.l > s.end.l) || (s.start.l == s.end.l && s.start.o > s.end.o) {
-		return s.end, s.start
-	}
-	return s.start, s.end
+	return s.rng.MinMax()
+}
+
+func (s *Selection) Contains(p Point) bool {
+	return s.rng.Contains(p)
 }
 
 func (s *Selection) Data() string {
@@ -62,17 +45,4 @@ func (s *Selection) Data() string {
 		return ""
 	}
 	return s.text.DataInside(s.MinMax())
-}
-
-func (s *Selection) Contains(p Point) bool {
-	min, max := s.MinMax()
-	if min.l <= p.l && p.l <= max.l {
-		if p.l == min.l && p.o < min.o {
-			return false
-		} else if p.l == max.l && p.o >= max.o {
-			return false
-		}
-		return true
-	}
-	return false
 }
