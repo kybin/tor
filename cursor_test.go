68a3398536e49074e62990ccf3fbbe4d053b4db1
kim yongbin <kybinz@gmail.com>
2017-12-14 12:44:48 +0900

    fix cursor test

--- cursor_test.go
+++ cursor_test.go
@@ -6,35 +6,40 @@ import (
 
 func TestBFromO(t *testing.T) {
 	cases := []struct {
-		l    string
-		in   int
-		want int
+		line     string
+		o        int
+		tabWidth int
+		want     int
 	}{
 		{
-			"		yo, how you doin?",
-			9,
-			3,
+			line: "		yo, how you doin?",
+			o:        9,
+			tabWidth: 4,
+			want:     3,
 		},
 		{
-			"	func() xxx {",
-			10,
-			7,
+			line: "	func() xxx {",
+			o:        10,
+			tabWidth: 4,
+			want:     7,
 		},
 		{
-			"for o := viewer.min.o ; o < viewer.max.o ; o++ {",
-			10,
-			10,
+			line:     "for o := viewer.min.o ; o < viewer.max.o ; o++ {",
+			o:        10,
+			tabWidth: 4,
+			want:     10,
 		},
 		{
-			"	SetCell(l, o, ' ', term.ColorDefault, term.ColorDefault)",
-			11,
-			8,
+			line: "	SetCell(l, o, ' ', term.ColorDefault, term.ColorDefault)",
+			o:        11,
+			tabWidth: 4,
+			want:     8,
 		},
 	}
 	for _, c := range cases {
-		got := BFromO(c.l, c.in)
+		got := BFromO(c.line, c.o, c.tabWidth)
 		if got != c.want {
-			t.Errorf("BFromO(%v, %v) == %v, want %v", c.l, c.in, got, c.want)
+			t.Errorf("BFromO(%v, %v): got %v, want %v", c.line, c.o, got, c.want)
 		}
 	}
 }
