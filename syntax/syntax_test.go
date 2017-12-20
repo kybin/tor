7a0dade457ef5c3c5a05a5f8e69dc9e68a55e644
kim yongbin <kybinz@gmail.com>
2017-12-20 16:43:03 +0900

    syntax: make a complete test
    not just print it.

--- syntax/syntax_test.go
+++ syntax/syntax_test.go
@@ -1,23 +1,58 @@
 package syntax
 
 import (
-	"fmt"
-	"io/ioutil"
+	"reflect"
 	"testing"
 )
 
 func TestUsage(t *testing.T) {
-	testText, err := ioutil.ReadFile("testdata/test.txt")
-	if err != nil {
-		t.Fatal(err)
+	cases := []struct {
+		text     []byte
+		langName string
+		want     []Match
+	}{
+		{
+			text: []byte(`package main
+
+// this is a comment.
+
+/*
+	this is a multi-line comment.
+	that has multiple lines.
+		with irregular indents.
+*/
+
+func main() {
+	s := "yo"	  
+	t := "\"string\" inside of a string"
+	r := 'rune'
+	
+}
+`),
+			langName: "go",
+			want: []Match{
+				{"package", Pos{0, 0}, Pos{0, 8}},
+				{"comment", Pos{2, 0}, Pos{2, 21}},
+				{"multi line comment", Pos{4, 0}, Pos{8, 2}},
+				{"string", Pos{11, 6}, Pos{11, 10}},
+				{"trailing spaces", Pos{11, 10}, Pos{11, 13}},
+				{"string", Pos{12, 6}, Pos{12, 9}},
+				{"string", Pos{12, 16}, Pos{12, 37}},
+				{"rune", Pos{13, 6}, Pos{13, 12}},
+				{"trailing spaces", Pos{14, 0}, Pos{14, 1}},
+			},
+		},
 	}
-	lang, ok := Languages["go"]
-	if !ok {
-		return
-	}
-	matches := lang.Parse(testText)
-	for _, m := range matches {
-		fmt.Println(m)
+	for _, c := range cases {
+		lang, ok := Languages[c.langName]
+		if !ok {
+			return
+		}
+		got := lang.Parse(c.text)
+		if !reflect.DeepEqual(got, c.want) {
+			t.Fatalf("(%v).Parse(%v): got %v, want %v", lang, c.text, got, c.want)
+		}
+
 	}
 }
 
