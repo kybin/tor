fc326e7172e66f3f5818e43971ad2ad7a9238357
kim yongbin <kybinz@gmail.com>
2018-01-03 19:57:59 +0900

    remove exist, it is unused now.

--- file.go
+++ file.go
@@ -73,15 +73,3 @@ func save(f string, t *Text) error {
 	}
 	return nil
 }
-
-// exists returns whether the given file or directory exists or not
-func exists(path string) (bool, error) {
-	_, err := os.Stat(path)
-	if err == nil {
-		return true, nil
-	}
-	if os.IsNotExist(err) {
-		return false, nil
-	}
-	return false, err
-}
